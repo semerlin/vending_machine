767cc78bacf0d77104be84d8a51ded490dc000c8
Elious Huang <elious.huang@gmail.com>
2018-04-01 22:39:50 +0800

    add simple http module

---
+++ board/wifi/simple_http.c
@@ -0,0 +1,25 @@
+/**
+* This file is part of the vendoring machine project.
+*
+* Copyright 2018, Huang Yang <elious.huang@gmail.com>. All rights reserved.
+*
+* See the COPYING file for the terms of usage and distribution.
+*/
+#include "FreeRTOS.h"
+#include "simple_http.h"
+#include "esp8266.h"
+
+/* default timeout time */
+#define DEFAULT_TIMEOUT      (3000 / portTICK_PERIOD_MS)
+
+const char set_page[] ="<html><body><a href=\"/ON\"><input type=\"Button\" value=\"ON\" \
+style=\"position:absolute;top:50%;left:40%\"></input></a><a href=\"/OFF\"><input type=\"Button\" value=\"OFF\" \
+style=\"position:absolute;top:50%;left:60%\"></input></a></body></html>";
+/**
+ * @brief pms5003 data process task
+ * @param serial handle
+ */
+int http_init(void)
+{
+    return esp8266_setmode(AP, DEFAULT_TIMEOUT);
+}
