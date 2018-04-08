3f54df66ec005fd207fc2baa387fc5f24aeb1af9
Elious Huang <elious.huang@gmail.com>
2018-04-08 23:03:10 +0800

    complete led interface

--- board/led_net.c
+++ board/led_net.c
@@ -8,6 +8,33 @@
 #include "led_motor.h"
 #include "FreeRTOS.h"
 #include "timers.h"
+#include "assert.h"
+#include "trace.h"
+#include "pinconfig.h"
+
+#undef __TRACE_MODULE
+#define __TRACE_MODULE  "[led_net]"
+
+#define LED_NUM  3
+
+/* led name */
+const char *led_names[LED_NUM] = {"LED1", "LED2", "LED3"};
+/* timer handler */
+TimerHandle_t led_timers[LED_NUM] = {NULL, NULL, NULL};
+
+/**
+ * @brief timer callback
+ * @param pxTimer - timer handler
+ */
+static void vTimerCallback(TimerHandle_t pxTimer)
+{
+    int32_t led_num;
+    
+    /* Which timer expired? */
+    led_num = (int32_t)pvTimerGetTimerID(pxTimer);
+
+    pin_toggle(led_names[led_num]);
+}
 
 /**
  * @brief turn on led
@@ -15,6 +42,8 @@
  */
 void led_net_turn_on(uint8_t num)
 {
+    assert_param(num < LED_NUM);
+    pin_set(led_names[num]);
 }
 
 /**
@@ -23,6 +52,8 @@ void led_net_turn_on(uint8_t num)
  */
 void led_net_turn_off(uint8_t num)
 {
+    assert_param(num < LED_NUM);
+    pin_reset(led_names[num]);
 }
 
 /**
@@ -32,6 +63,30 @@ void led_net_turn_off(uint8_t num)
  */
 void led_net_flashing(uint8_t num, TickType_t interval)
 {
+    assert_param(num < LED_NUM);
+    if (NULL != led_timers[num])
+    {
+        if (pdPASS != xTimerStart(led_timers[num], 0))
+        {
+            TRACE("start timer failed\n");
+        }
+    }
+    else
+    {
+        led_timers[num] = xTimerCreate("Timer", interval, pdTRUE, (void *)num,
+                                    vTimerCallback);
+        if (NULL == led_timers[num])
+        {
+            TRACE("create timer failed\n");
+        }
+        else
+        {
+            if (pdPASS != xTimerStart(led_timers[num], 0))
+            {
+                TRACE("start timer failed\n");
+            }
+        }
+    }
 }
 
 /**
@@ -40,4 +95,7 @@ void led_net_flashing(uint8_t num, TickType_t interval)
  */
 void led_net_stop_flashing(uint8_t num)
 {
+    assert_param(num < LED_NUM);
+    assert_param(NULL != led_timers[num]);
+    xTimerStop(led_timers[num], 0);
 }
